3d6552ef6f05e529195205af018f8ae94a8822a7
4925k <dibekpoudyal@gmail.com>
2021-12-18 12:31:49 +0545

    some kind of structure has been made. not in working state

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	Major  = "0"
+	Minor  = "1"
+	Fix    = "0"
+	Verbal = "txn add and balances list"
+)
+
+var versionCMD = &cobra.Command{
+	Use:   "version",
+	Short: "describes version info",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %s.%s.%s %s", Major, Minor, Fix, Verbal)
+	},
+}
